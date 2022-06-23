c6552b36485ed19e7522e6c3db76f58b8b2c7aa0
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2022-06-23 23:58:56 +0700

    Draw pixel but not smooth

--- PixelArtMaker/main.js
+++ PixelArtMaker/main.js
@@ -1,6 +1,16 @@
 
+/* ================ VARIABLE ================ */
+
 const container = document.querySelector('.pixel__container'),
-      colorList = document.querySelectorAll('.pixel__gallery .color');
+      colorList = document.querySelectorAll('.pixel__gallery .color'),
+      root = document.documentElement,
+      eraser = document.querySelector('.tool--erase'),
+      images = document.querySelectorAll('img'),
+      gallery = document.querySelector('.pixel__gallery');
+
+let currentColor;
+
+
 
 const popularColors = [
     "#F20505", "#05F2DB", "#F2CB05", "#F28705",
@@ -8,24 +18,18 @@ const popularColors = [
     "#BF04A0", "#BFBFBF", "#F2F2F2", "#02735E"
 ];
 
-[...colorList].forEach((c, i) => c.style.backgroundColor = popularColors[i]);
+
+/* ================ HANDLE EVENT ================ */
 
 const createGrid = () => {
     const width = Number.parseInt(window.getComputedStyle(container).width) / 10;
     const height = Number.parseInt(window.getComputedStyle(container).height) / 10;
     
-    let first, second;
+    let first = 'white', second = 'grey';
     console.log(height * width);
     for(let i = 0; i < height; i++) {
-        if(i % 2) {
-            first = "white";
-            second = "grey";
-        }
-        else {
-            first = "grey";
-            second = "white";
-        }
-
+        // each even or odd line will change the order in which it appears white or gray
+        [first, second] = [second, first];
         for(let j = 0; j < width; j++) {
             container.insertAdjacentHTML('beforeend', 
                 `
@@ -33,7 +37,52 @@ const createGrid = () => {
                 `
             );
         }
+    
     }
+
 }
 
-createGrid();
+const init = () => {
+    currentColor = '#000';
+
+    // Create colors table 
+    [...colorList].forEach((c, i) => c.style.backgroundColor = popularColors[i]);
+    
+    // Create grid area for painting
+    createGrid();
+    
+}
+
+// const activatedMouseover = 
+
+const paint = e => {
+    e.preventDefault();
+
+    const box = e.target;
+    if(!box.classList.contains('pixel__box'))    return;
+    
+    box.classList.add('active');
+}
+
+const pickColor = e => {
+    if(!e.target.classList.contains('color')) return;
+
+    currentColor = window.getComputedStyle(e.target).backgroundColor;
+    document.documentElement.style.setProperty('--color-current', currentColor);
+    // console.log(currentColor);
+}
+
+// use mousemove event instead of mouseover because mouse will change the first element while mouseover will change the second element.
+const activatedMousemove = () => container.addEventListener('mousemove', paint);
+const unActivatedMousemove = () => container.removeEventListener('mousemove', paint);
+
+window.addEventListener('load', init);
+
+container.addEventListener('mousedown', activatedMousemove);
+container.addEventListener('mouseup', unActivatedMousemove);
+container.addEventListener('mouseleave', unActivatedMousemove);
+container.addEventListener('click', paint);
+container.addEventListener('contextmenu', e => e.preventDefault());
+
+
+gallery.addEventListener('click', pickColor);
