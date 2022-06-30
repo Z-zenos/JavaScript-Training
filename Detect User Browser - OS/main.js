25ad57b624bfe0d699f5416707bfed160bc9e1a7
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2022-06-30 11:01:27 +0700

    Detect User Browser & OS complete

---
+++ Detect User Browser - OS/main.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/* ========== VARIABLES ========== */
+
+// Get info about user browser
+const userAgent = navigator.userAgent;
+
+// Determine user browser
+let browser;
+if(userAgent.match(/edge/i))
+    browser = 'edge';
+else if(userAgent.match(/firefox|fxios/i))
+    browser = 'firefox';
+else if(userAgent.match(/chrome|chromium|crios/i))
+    browser = 'chrome';
+else if(userAgent.match(/opr/i))
+    browser = 'opera'
+else if(userAgent.match(/safari/i))
+    browser = 'safari';
+else 
+    browser = 'Alien browser';
+
+// Determine user operation system
+let os;
+if(userAgent.match(/linux/i))
+    os = 'linux';
+else if(userAgent.match(/window/i))
+    os = 'window';
+else if(userAgent.match(/mac/i))
+    os = 'macos';
+
+const init = () => {
+    document.querySelector(`.${browser}`).classList.add('active');
+    document.querySelector(`.${os}`).classList.add('active');
+}
+
+window.addEventListener('load', init);
