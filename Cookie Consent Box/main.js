a36ff5ecbabedf6d1b10ead9d7c4be51c69b992e
Z-techIT <tuan.ha194705@sis.hust.edu.vn>
2022-07-05 13:53:07 +0700

    Add expited day cookie

--- Cookie Consent Box/main.js
+++ Cookie Consent Box/main.js
@@ -12,7 +12,10 @@ const init = () => {
 }
 
 const eatCookie = () => {
-    document.cookie = `creator: z-codev;`;
+    // Add expire date is tomorrow from setted
+    const now = new Date();
+    const tomorrow = now.setDate(now.getDate() + 1);
+    document.cookie = `username: z-codev; expires=${new Date(tomorrow)}`;
     if(document.cookie)
         cookieBox.classList.remove('cookie--active');
     else alert("Cookie can't be set! Please unblock this site from the cookie setting of your browser.");
@@ -20,3 +23,12 @@ const eatCookie = () => {
 
 btnEat.addEventListener('click', eatCookie);
 window.addEventListener('load', init);
+
+/*
+    * Cookies are data, stored in small text files, on your computer.
+    * When a web server has sent a web page to a browser, the connection is shut down, and the server forgets everything about the user.
+    * Cookies were invented to solve the problem "how to remember information about the user":
+    * When a user visits a web page, his/her name can be stored in a cookie.
+    * Next time the user visits the page, the cookie "remembers" his/her name.
+    * When a browser requests a web page from a server, cookies belonging to the page are added to the request. This way the server gets the necessary data to "remember" information about users.
+*/
